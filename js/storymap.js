f1691e23782f17810782a8a9e146a13905b25027
Fixxy <Fixxy@users.noreply.github.com>
2021-01-27 15:52:50 +0300

    Update storymap.js
    only update the map if parameters were provided

--- js/storymap.js
+++ js/storymap.js
@@ -3,5 +3,5 @@ if (typeof (mapFrame) != 'undefined' && mapFrame != null) {
     let params = new URLSearchParams(window.location.search);
     let lang = params.get('lang');
     let storyID = params.get('id');
-    mapFrame.src = `https://maps.greenpeace.org/maps/U4E/?lang=${lang?lang:''}${storyID?`&id=${storyID}`:''}`;
+    if (lang || storyID) mapFrame.src = `https://maps.greenpeace.org/maps/U4E/?${lang?`lang=${lang}`:''}${storyID?`&id=${storyID}`:''}`;
 }
