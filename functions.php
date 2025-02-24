f0d2c06b4aab3a88aa7e8babec6f5d398ffdfb1e
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-02-24 16:10:06 +0200

    Move FundraiseUp script to template

--- functions.php
+++ functions.php
@@ -3,26 +3,8 @@
 /**
  * Additional code for the child theme goes in here.
  */
-function add_inline_script_for_specific_page() {
-if (is_page(12401) || is_page(12376)) { // Replace 'your-page-slug' with the actual slug or ID
-        ?>
-        <script>
-            <!-- Fundraise Up: the new standard for online giving -->
-<script>(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'
-.split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
-:function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
-j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a+'';
-t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=5;o.h=w.location.href;o.l=[];
-for(i=0;i<8;i++)o[l[i]]=o(l[i]);w[n]=o}
-})(window,document,'script','FundraiseUp','AGKKXWSM');</script>
-<!-- End Fundraise Up -->
-        </script>
-        <?php
-    }
-}
-add_action('wp_head', 'add_inline_script_for_specific_page');
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
+ add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
 
 function enqueue_child_styles() {
 	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
